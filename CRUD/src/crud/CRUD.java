220a01fd3310795447f6458bc2ee594d67dadb84
IrvingAZZA <ivg.azza.92@gmail.com>
2024-12-20 16:58:31 -0500

    IMPLEMENTACION DE FUNCIONES

--- CRUD/src/crud/CRUD.java
+++ CRUD/src/crud/CRUD.java
@@ -3,6 +3,8 @@
  * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
  */
 package crud;
+import java.util.List;
+import java.util.Scanner;
 
 /**
  *
@@ -15,6 +17,58 @@ public class CRUD {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        UsuarioDAO usuariodao = new UsuarioDAO();
+        Scanner scanner  = new Scanner(System.in);
+        
+        int opcion;
+        do {            
+            System.out.println("\n--- Menú CRUD ---");
+            System.out.println("1. Agregar usuario");
+            System.out.println("2. Ver usuarios");
+            System.out.println("3. Actualizar usuario");
+            System.out.println("4. Eliminar usuario");
+            System.out.println("5. Salir");
+            System.out.print("Elige una opción: ");
+            opcion = scanner.nextInt();
+            scanner.nextLine();
+            
+            switch (opcion) {
+                case 1:
+                    System.out.print("ID: ");
+                    String id = scanner.nextLine();
+                    System.out.print("NOMBRE: ");
+                    String nombre = scanner.nextLine();
+                    System.out.print("country: ");
+                    String countr = scanner.nextLine();
+                    System.out.print("CODE: ");
+                    String code = scanner.nextLine();
+                    System.out.print("WEB: ");
+                    String web = scanner.nextLine();
+                    
+                    if (usuariodao.agregarLinea(id,nombre,countr,code,web)) 
+                    {
+                        System.out.println("INGRESADO CORRECTAMENTE ");
+                    }
+                    break;
+                case 2:
+                    List<String> usuarios = usuariodao.Mostrar_Tabla();
+                    if(usuarios.isEmpty())
+                    {
+                        System.out.println("NO hay usuario registrados. ");
+                    }else
+                    {
+                        System.out.println("\n---la lista es: ----");
+                        usuarios.forEach(System.out::println);
+                    }
+                    break;
+                case 3:
+                    System.out.println("el ID a actualizar es: ");
+                    String idactualizar = scanner.nextLine();
+                    
+                default:
+                    throw new AssertionError();
+            }
+        } while (true);
     }
     
 }
--- CRUD/src/crud/UsuarioDAO.java
+++ CRUD/src/crud/UsuarioDAO.java
@@ -29,4 +29,65 @@ public class UsuarioDAO {
             return false;
         }
     }
+    
+    // MOSTRAR
+    public List<String> Mostrar_Tabla()
+    {
+        String query = "SELECT * FROM list_of_universities";
+        List<String> usuarios = new ArrayList<>();
+        try 
+           (Connection conecion = conexion.getConnection();
+           Statement stat = conecion.createStatement();
+           ResultSet rs = stat.executeQuery(query)){
+           
+           while (rs.next())
+           {
+               System.out.println(" EL ID ES: " + rs.getString("COL 1"));
+               System.out.println(" EL NOMBRE ES: " + rs.getString("COL 2"));
+               System.out.println(" EL COUNTRY ES: " + rs.getString("COL 3"));
+               System.out.println(" EL CODE ES: " + rs.getString("COL 4"));
+               System.out.println(" EL WEB ES: " + rs.getString("COL 5"));
+               System.out.println("----------------------------------------");
+           }
+        } catch (Exception e) {
+            System.err.println("error al mostrar: " + e.getMessage());
+        }
+        return usuarios;
+    }
+    //ACTUALIZAR
+    public boolean actualizar_lista(String id, String name, String country, String code, String web)
+    {
+        String query = "UPDATE list_of_universities SET COL 1 = ?,COL 2 = ?, COL 3 = ?, COL 4 = ?, COL 5 = ? WHERE COL 1 = ?";
+        
+        try (Connection conecion = conexion.getConnection();
+                PreparedStatement pst = conecion.prepareStatement(query)) 
+        {
+            pst.setString(1, id);
+            pst.setString(2, name);
+            pst.setString(3, country);
+            pst.setString(4, code);
+            pst.setString(5, web);
+            pst.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.err.println("no se puedo actualizar la lista: "+ e.getMessage());
+            return false;
+        }
+    }
+    //BORRAR
+    public boolean Eliminar_registro(String id)
+    {
+        String query = "DELETE FROM list_of_universities WHERE COL 1 = ?";
+        try (Connection conecion = conexion.getConnection();
+                PreparedStatement pst = conecion.prepareStatement(query))
+        {
+            pst.setString(1, id);
+            pst.executeUpdate();
+            return true;
+        } catch (Exception e) {
+            System.err.println("nO SE PUDO BORRAR EL REGISTRO: "+e.getMessage());
+            return false;
+        }
+    }
+    
 }
